b27eda20b47aceb892e10e6f5dd31879f68a3a2d
Sazzad Hussain <sazzadh88@gmail.com>
2022-03-24 00:05:15 +0530

    Class name typo fix

--- pages/movie/[id]/index.js
+++ pages/movie/[id]/index.js
@@ -10,7 +10,7 @@ const Movie = ({ movie }) => {
       <div className="px-3">
         <Image src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} width={1000} height={600} className="rounded-md" alt={movie.title} />
         <h1 className="font-bold text-xl my-2">{movie.title}</h1>
-        <p className="tetx-gray-600 text-sm mt-4">{movie.overview}</p>
+        <p className="text-gray-600 text-sm mt-4">{movie.overview}</p>
         <p className="mt-5 text-gray-600 text-sm">Genres: <span className="font-bold">{movie.genres.map(genre => genre.name).join(', ')}</span></p>
         <p className="text-gray-600 text-sm">Release Date: <span className="font-bold">{movie.release_date}</span></p>
       </div>
